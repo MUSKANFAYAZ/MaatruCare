7ed5caf239d1bc015e7707d690afa76d2fd8182d
MUSKANFAYAZ <muskanfayaz48@gmail.com>
2025-11-23 18:58:26 +0530

    Feat: Complete authentication with Google and Facebook

---
+++ backend/models/User.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["mother", "professional"],
+      default: "mother",
+    },
+    authProvider: {
+      type: String,
+      enum: ["email", "google", "facebook"],
+      default: "email",
+    },
+    isVerified: {
+    type: Boolean,
+    default: false 
+  },
+  otp: {
+    type: String,
+  },
+  otpExpires: {
+    type: Date,
+  }
+  },
+  { timestamps: true,
+    collection: 'MamaMindSense'
+   }
+);
+
+module.exports = mongoose.model("User", UserSchema);
